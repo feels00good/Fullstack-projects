022f3fd6f52304fb49dd4c363b51ac03b956ff8e
feels00good <feels00good@ka-as.ru>
2019-11-08 11:51:01 +0300

    connecting to database using sequelize

--- server/src/app.js
+++ server/src/app.js
@@ -3,22 +3,18 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
+const {sequelize} = require('./models')
+const config = require('./config/config')
 
 const app = express()
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/status', (req, res) => {
-  res.send({
-    message: 'Hello world'
-  })
-})
+require('./routes')(app)
 
-app.post('/register', (req, res) => {
-  res.send({
-    message: `Hello ${req.body.email} Your user was registered! Have fun!`
+sequelize.sync()
+  .then(() => {
+    app.listen(config.port)
+    console.log(`Server started om port ${config.port}`)
   })
-})
-
-app.listen(process.env.PORT || 8081)
